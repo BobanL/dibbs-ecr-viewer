020d0a1ffc738ac362d9e35ef6925b8256ff7405
Mary McGrath <m.c.mcgrath13@gmail.com>
2025-03-28 12:01:16 -0400

    feat: animate expand

--- containers/ecr-viewer/src/app/components/RespectMotionPreferences.tsx
+++ containers/ecr-viewer/src/app/components/RespectMotionPreferences.tsx
@@ -7,7 +7,7 @@ import { MotionConfig } from "motion/react";
  * Ensure motion preferences are respected
  * @param props react props
  * @param props.children child components
- * @returns component to make sure reduced motion preferences are respected 
+ * @returns component to make sure reduced motion preferences are respected
  */
 const RespectMotionPreferences = ({
   children,
